19d4567ed2556f21741fcab022bd2bd62c600333
fdgenie <pa.aris@yahoo.gr>
2021-04-24 19:32:38 +0300

    contact page fixes

---
+++ src/components/inputForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-input
+    outlined
+    :label="label"
+    label-color="white"
+    color="white"
+    dark
+    :type="type"
+    :value="model"
+    @input="$emit('update:model', $event)"
+  />
+</template>
+
+<script>
+export default {
+  name: "inputForm",
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    model: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,169 +1,48 @@
 <template>
   <q-page class="q-pa-md bg-grey-10">
-    <div v-if="!$q.platform.is.mobile" class="column" style="height: calc(100vh - 50px);" >
-      <div class="col-3" />
-      <div class="col-4 row">
-        <span class="col-1" />
-        <span class="col">
-          <p class="text-h2 text-teal-13">
-            Get in touch!
-          </p>
-          <p class="text-h5 text-grey-4">
-            Available for freelance work or collaborations
-          </p>
-          <q-form
-            v-if="!loading"
-            @submit.prevent="sendEmail"
-            class="q-mt-xl contact-form"
-          >
-            <span class="row">
-              <span class="col q-mr-md">
-                <q-input
-                  outlined
-                  label="Name"
-                  label-color="white"
-                  color="white"
-                  dark
-                  type="text"
-                  name="user_name"
-                  v-model="to_name"
-                />
-              </span>
-              <span class="col">
-                <q-input
-                  outlined
-                  label="E-Mail"
-                  label-color="white"
-                  color="white"
-                  dark
-                  type="email"
-                  name="user_email"
-                  v-model="from_name"
-                />
-              </span>
-            </span>
-            <span class="row q-mt-md">
-              <span class="col">
-                <q-input
-                  type="textarea"
-                  outlined
-                  label="Message"
-                  label-color="white"
-                  color="white"
-                  dark
-                  name="message"
-                  v-model="message"
-                />
-              </span>
-            </span>
-            <span class="row q-mt-md">
-              <span class="col">
-                <q-btn type="submit" value="send" color="teal-13">
-                  Submit
-                </q-btn>
-              </span>
-            </span>
-          </q-form>
-          <span v-else class="text-h5 text-grey-4">
-            <span v-if="!sentMessage">
-              <q-icon name="mdi-loading mdi-spin" class="q-mr-sm" />
-              Sending Message...
-            </span>
-            <span v-else>
-              <q-icon name="mdi-check-circle-outline" />
-              Your message has been sent!
-            </span>
-          </span>
+    <div class="center-left-text">
+      <p :class="$q.screen.gt.sm ? 'text-h2' : 'text-h4'" class="text-teal-13">
+        Get in touch!
+      </p>
+      <p class="text-h5 text-grey-4">
+        Available for freelance work or collaborations
+      </p>
+      <q-form
+        v-if="!loading"
+        @submit.prevent="sendEmail"
+        class="q-mt-xl q-gutter-md"
+      >
+        <InputForm label="Name" type="text" :model.sync="to_name" />
+        <InputForm label="E-Mail" type="email" :model.sync="from_name" />
+        <InputForm label="Message" type="textarea" :model.sync="message" />
+
+        <q-btn type="submit" value="send" color="teal-13">
+          Submit
+        </q-btn>
+      </q-form>
+      <span v-else class="text-h5 text-grey-4">
+        <span v-if="!sentMessage">
+          <q-icon name="mdi-loading mdi-spin" class="q-mr-sm" />
+          Sending Message...
         </span>
-        <span class="col-5" />
-      </div>
-      <div class="col" />
-    </div>
-    <div v-else class="column" style="height: calc(100vh - 50px);">
-      <div class="col-1" />
-      <div class="col-4 row">
-        <span class="col">
-          <p class="text-h4 text-teal-13">
-            Get in touch!
-          </p>
-          <p class="text-h6 text-grey-4">
-            Available for freelance work or collaborations
-          </p>
-          <q-form
-            v-if="!loading"
-            @submit.prevent="sendEmail"
-            class="q-mt-xl contact-form"
-          >
-            <span class="row">
-              <span class="col q-mr-md">
-                <q-input
-                  outlined
-                  label="Name"
-                  label-color="white"
-                  color="white"
-                  dark
-                  type="text"
-                  name="user_name"
-                  v-model="to_name"
-                />
-              </span>
-              <span class="col">
-                <q-input
-                  outlined
-                  label="E-Mail"
-                  label-color="white"
-                  color="white"
-                  dark
-                  type="email"
-                  name="user_email"
-                  v-model="from_name"
-                />
-              </span>
-            </span>
-            <span class="row q-mt-md">
-              <span class="col">
-                <q-input
-                  type="textarea"
-                  outlined
-                  label="Message"
-                  label-color="white"
-                  color="white"
-                  dark
-                  name="message"
-                  v-model="message"
-                />
-              </span>
-            </span>
-            <span class="row q-mt-md">
-              <span class="col">
-                <q-btn type="submit" value="send" color="teal-13">
-                  Submit
-                </q-btn>
-              </span>
-            </span>
-          </q-form>
-          <span v-else class="text-h5 text-grey-4">
-            <span v-if="!sentMessage">
-              <q-icon name="mdi-loading mdi-spin" class="q-mr-sm" />
-              Sending Message...
-            </span>
-            <span v-else>
-              <q-icon name="mdi-check-circle-outline" />
-              Your message has been sent!
-            </span>
-          </span>
+        <span v-else>
+          <q-icon name="mdi-check-circle-outline" />
+          Your message has been sent!
         </span>
-      </div>
-      <div class="col" />
+      </span>
     </div>
   </q-page>
 </template>
 
 <script>
 import emailjs from "emailjs-com";
+import InputForm from "src/components/inputForm.vue";
 
 export default {
   name: "ContactsPage",
+  components: {
+    InputForm
+  },
   data() {
     return {
       to_name: "",
@@ -174,10 +53,10 @@ export default {
     };
   },
   methods: {
-    sendEmail(e) {
+    sendEmail() {
       this.loading = true;
 
-      let test = {
+      let customerInfo = {
         to_name: this.to_name,
         from_name: this.from_name,
         message: this.message
@@ -187,33 +66,41 @@ export default {
         .send(
           "service_e6fbpkh",
           "template_iv21o1b",
-          test,
+          customerInfo,
           "user_dkbfABjZxKU5dSCe9RLl4"
         )
-        .then(
-          result => {
-            this.to_name = "";
-            this.from_name = "";
-            this.message = "";
-            this.sentMessage = true;
+        .then(() => {
+          this.sentMessage = true;
+        })
+        .catch(() => {
+          this.$q.notify({
+            message: "Something didn't go well. Please try again in a while.",
+            color: "red",
+            position: "top-right"
+          });
+        })
+        .finally(() => {
+          this.to_name = "";
+          this.from_name = "";
+          this.message = "";
 
-            setTimeout(() => {
-              this.sentMessage = false;
-              this.loading = false;
-            }, 4000);
-
-            this.$store.dispatch("user/saveFavoriteClass", {
-              name: this.classHero.index,
-              url: this.classHero.url
-            });
-          },
-          error => {
-            console.log("FAILED...", error);
-          }
-        );
+          setTimeout(() => {
+            this.sentMessage = false;
+            this.loading = false;
+          }, 4000);
+        });
     }
   }
 };
 </script>
 
-<style></style>
+<style scoped>
+@media screen and (min-width: 996px) {
+  .center-left-text {
+    position: absolute;
+    top: 25%;
+    right: 40%;
+    left: 10%;
+  }
+}
+</style>
